c2ae206dc233a0e4ecd65d6d3b76638e41574a38
RyanValdivia <rvaldiviase@unsa.edu.pe>
2024-08-11 10:52:53 -0500

    add configuration about PasswordEncoder and Authentication

--- backend/src/main/java/gogodocs/backend/models/users/UsersRepository.java
+++ backend/src/main/java/gogodocs/backend/models/users/UsersRepository.java
@@ -2,5 +2,8 @@ package gogodocs.backend.models.users;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface UsersRepository extends MongoRepository<Users, String> {
+    public Optional<Users> findByUsername(String username);
 }
