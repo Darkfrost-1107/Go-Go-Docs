36973a3097e684ed93622e83b927f05d41ff1a82
RyanValdivia <rvaldiviase@unsa.edu.pe>
2024-08-11 11:30:25 -0500

    add Service to Login and Register

---
+++ backend/src/main/java/gogodocs/backend/auth/AuthService.java
@@ -0,0 +1,50 @@
+package gogodocs.backend.auth;
+
+import gogodocs.backend.jwt.JwtService;
+import gogodocs.backend.models.users.Users;
+import gogodocs.backend.models.users.UsersRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional (rollbackFor = Exception.class)
+@RequiredArgsConstructor
+@Service
+public class AuthService {
+    private final UsersRepository usersRepository;;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthResponse login (LoginRequest loginRequest) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken
+                (loginRequest.getUsername(), loginRequest.getPassword()));
+        UserDetails user = usersRepository.findByUsername(loginRequest.getUsername())
+                .orElseThrow(() -> new UsernameNotFoundException
+                        ("User with username " + loginRequest.getUsername() + " not found"));
+        String token = jwtService.getToken(user);
+        
+        return AuthResponse.builder()
+                .token(token)
+                .build();
+    }
+
+    public AuthResponse register (RegisterRequest registerRequest) {
+        Users user = Users.builder()
+                .username(registerRequest.getUsername())
+                .password(passwordEncoder.encode(registerRequest.getPassword()))
+                .imageURL(registerRequest.getImageURL())
+                .email(registerRequest.getEmail())
+                .build();
+
+        usersRepository.save(user);
+        return AuthResponse.builder()
+                .token(jwtService.getToken(user))
+                .build();
+    }
+}
