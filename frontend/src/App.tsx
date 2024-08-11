ce7d9cc528688f518b7d5829a10d3e1842c6f1a3
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2024-08-10 23:45:27 -0500

    add frontend config

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { Button } from '@/components/ui/button'
+
+function App() {
+  return (
+    <>
+      <div>
+        <Button>
+          hola
+        </Button>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
